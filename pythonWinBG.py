e432c4a1d5db640e15708d9af83782899327f9bb
Bryan Tait <88522137+Bryan0x05@users.noreply.github.com>
2023-02-20 22:59:34 -0500

    prototype for linux/windows

---
+++ pythonWinBG.py
@@ -0,0 +1,13 @@
+import ctypes # python c wrapper function
+import sys # for command line
+
+def changeBG(imagepath):
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, imagepath , 0)
+    return
+
+
+try:
+    imagepath = sys.argv[1]
+    changeBG(imagepath)
+except:
+    print("Couldn't find filepath")
